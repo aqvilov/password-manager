8e59ea0f2a280e4e5334b43657580387843da120
Ars <arseniyrus15@gmail.com>
2025-12-02 01:38:14 +0300

    added decrypt function

--- modules/encryption.go
+++ modules/encryption.go
@@ -37,10 +37,76 @@ func (pm *PasswordManager) Encrypt(plaintext string) (string, error) {
 	return base64.StdEncoding.EncodeToString(ciphertext), nil
 }
 
-func (pm *PasswordManager) Decrypt(ciphertext string) (string, error) {
-	cipher, err := base64.StdEncoding.DecodeString(ciphertext)
+//довести до конца
+
+package modules
+
+import (
+"crypto/aes"
+"crypto/cipher"
+"crypto/rand"
+"encoding/base64"
+"fmt"
+"io"
+)
+
+// Encrypt шифрует строку с ключом
+func Encrypt(plaintext string, key []byte) (string, error) {
+	plaintextBytes := []byte(plaintext)
+
+	block, err := aes.NewCipher(key)
 	if err != nil {
-		return "", fmt.Errorf("ошибка декодировки: %v", err)
+		return "", fmt.Errorf("ошибка создания cipher: %v", err)
 	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", fmt.Errorf("ошибка создания GCM: %v", err)
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return "", fmt.Errorf("ошибка генерации nonce: %v", err)
+	}
+
+	ciphertext := gcm.Seal(nonce, nonce, plaintextBytes, nil) // ШИФРУЕМ
+	return base64.StdEncoding.EncodeToString(ciphertext), nil
+}
+
+
+// расшифровываем
+
+func Decrypt(encryptedText string, key []byte) (string, error) {
+	ciphertext, err := base64.StdEncoding.DecodeString(encryptedText)
+	if err != nil {
+		return "", fmt.Errorf("ошибка декодирования base64: %v", err)
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", fmt.Errorf("ошибка создания cipher: %v", err)
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", fmt.Errorf("ошибка создания GCM: %v", err)
+	}
+
+	nonceSize := gcm.NonceSize()
+
+	if len(ciphertext) < nonceSize {
+		return	"", fmt.Errorf("неверная длина ciphertext")
+	}
+
+	// извлекаем nonce
+	nonce := ciphertext[:nonceSize]
+	ciphertext = ciphertext[nonceSize:] // т.к мы знаем, что сначала nonce потом text
+
+	plaintext, err := gcm.Open(nil,nonce, ciphertext, nil)
+	if err != nil {
+		return "", err
+	}
+
+	return string(plaintext), nil
 
 }
