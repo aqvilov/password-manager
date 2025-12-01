7e6e3c36068129f5e18bb2256897df47eec4f09e
Ars <arseniyrus15@gmail.com>
2025-12-02 01:39:28 +0300

    encryption.go were fixed

--- modules/encryption.go
+++ modules/encryption.go
@@ -37,43 +37,6 @@ func (pm *PasswordManager) Encrypt(plaintext string) (string, error) {
 	return base64.StdEncoding.EncodeToString(ciphertext), nil
 }
 
-//довести до конца
-
-package modules
-
-import (
-"crypto/aes"
-"crypto/cipher"
-"crypto/rand"
-"encoding/base64"
-"fmt"
-"io"
-)
-
-// Encrypt шифрует строку с ключом
-func Encrypt(plaintext string, key []byte) (string, error) {
-	plaintextBytes := []byte(plaintext)
-
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		return "", fmt.Errorf("ошибка создания cipher: %v", err)
-	}
-
-	gcm, err := cipher.NewGCM(block)
-	if err != nil {
-		return "", fmt.Errorf("ошибка создания GCM: %v", err)
-	}
-
-	nonce := make([]byte, gcm.NonceSize())
-	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
-		return "", fmt.Errorf("ошибка генерации nonce: %v", err)
-	}
-
-	ciphertext := gcm.Seal(nonce, nonce, plaintextBytes, nil) // ШИФРУЕМ
-	return base64.StdEncoding.EncodeToString(ciphertext), nil
-}
-
-
 // расшифровываем
 
 func Decrypt(encryptedText string, key []byte) (string, error) {
@@ -95,14 +58,14 @@ func Decrypt(encryptedText string, key []byte) (string, error) {
 	nonceSize := gcm.NonceSize()
 
 	if len(ciphertext) < nonceSize {
-		return	"", fmt.Errorf("неверная длина ciphertext")
+		return "", fmt.Errorf("неверная длина ciphertext")
 	}
 
 	// извлекаем nonce
 	nonce := ciphertext[:nonceSize]
 	ciphertext = ciphertext[nonceSize:] // т.к мы знаем, что сначала nonce потом text
 
-	plaintext, err := gcm.Open(nil,nonce, ciphertext, nil)
+	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
 	if err != nil {
 		return "", err
 	}
